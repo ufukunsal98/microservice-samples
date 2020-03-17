f07cf51ff4bddc967ac678c6272e6dfa82c3f9ef
Ufuk Remzi <uufk-selen@hotmail.com>
2020-03-17 19:07:01 +0300

    Initial commit

---
+++ account-provider-api/src/main/java/com/ufuk/accountprovider/AccountProvider/AccountProviderApplication.java
@@ -0,0 +1,12 @@
+package com.ufuk.accountprovider.AccountProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountProviderApplication.class , args);
+    }
+}
